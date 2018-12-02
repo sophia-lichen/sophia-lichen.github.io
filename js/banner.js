36158ba28029cef255654c3dab7a1cdf7f59b332
Sophia Li <sophia_lichen@hotmail.com>
2018-12-02 20:31:47 +1100

    init

---
+++ js/banner.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("vangogh");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
